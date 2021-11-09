776d0831f61890e46c0eb974c605bccd6fe6190c
Aurelio Pita <jaapaurelio@gmail.com>
2021-11-09 08:41:24 +0000

    Fix authors

--- gatsby-node.js
+++ gatsby-node.js
@@ -106,6 +106,7 @@ exports.createSchemaCustomization = ({ actions }) => {
       title: String
       description: String
       date: Date @dateformat
+      author: AuthorsJson @link(by: "username", from: "author")
     }
 
     type Fields {
